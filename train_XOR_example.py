553fd026df4e4152b6687cb92827c6781c34b9e6
jagh <jagh1729@gmail.com>
2018-11-25 17:54:12 +0100

    XOR example to use the DiagnoseNET Framework

--- train_XOR_example.py
+++ train_XOR_example.py
@@ -5,29 +5,37 @@ for training a MLP on a desktop machine
 
 import numpy as np
 
-from diagnosenet.layers import Relu, Linear
+from diagnosenet.layers import Relu, Softmax
+from diagnosenet.losses import CrossEntropy
+from diagnosenet.optimizers import Adam
 from diagnosenet.graphs import MLP
 from diagnosenet.executors import DesktopExecution
 
 
 #####################################################################
-## A XOR example is defined as:
-inputs = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
-targets = np.array([[1, 0], [0, 1], [0, 1], [1, 0]])
-input_size = 2; output_size = 2
+## A XOR example is defined by:    Entries      its  Output
+##                            [0, 0] or [1, 1]  ->   [1, 0]
+##                            [0, 1] or [1, 0]  ->   [0, 1]
+inputs = np.array([[0, 0], [1, 0], [0, 1], [1, 1],
+                   [0, 0], [1, 0], [0, 1], [1, 1]])
+targets = np.array([[1, 0], [0, 1], [0, 1], [1, 0],
+                    [1, 0], [0, 1], [0, 1], [1, 0]])
 
 
 #####################################################################
 ## A simple feedforward network is implemented to solve a XOR example
 ## 1) Define the stacked layers
-layers = [Relu(input_size, 5),
-            Relu(5, 2),
-            Linear(2, output_size)]
+staked_layers = [Relu(2, 32),
+                Relu(32, 32),
+                Softmax(32, 2)]
 
-## 2) Select the neural network graph
-model = MLP(layers, input_size, output_size)
+## 2) Select the neural network architecture
+model = MLP(input_size=2, output_size=2,
+            layers=staked_layers,
+            loss=CrossEntropy,
+            optimizer=Adam(lr=0.01))
 
 ## 3) Select the execution machine mode
-projection = DesktopExecution(model).memoryexecutor(inputs, targets)
+projection = DesktopExecution(model, max_epochs=100).memoryexecutor(inputs, targets)
 
 print("Projection: {}".format(projection))
