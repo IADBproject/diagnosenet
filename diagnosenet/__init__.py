28024d61ee679994f4819e71da5a0ddd29c05862
jagh <jagh1729@gmail.com>
2018-11-26 10:20:54 +0100

    Updated logger to build instances over the diagnosenet methods

--- diagnosenet/__init__.py
+++ diagnosenet/__init__.py
@@ -7,10 +7,10 @@ from __future__ import division
 from __future__ import print_function
 
 import logging
-from diagnosenet.loggingconf import Config
+from diagnosenet.logger import Config
 
 #######################################################################
-### Set general logging configuration for using in _dIAgnoseNET_DataMining
+### Set general logger configuration for using in diagnosenet methods
 logging_config = Config()
 logging_config._setup_logger('_DiagnoseNET_',
             str('.diagnosenet.log'), logging.DEBUG)
