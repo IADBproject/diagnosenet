0707940981b5a2efa255338bc1fb6f7837e2549d
jagh <jagh1729@gmail.com>
2018-12-15 17:15:02 +0100

    Updated datamanager for splitting, batching and multi-task dataset configuration

--- diagnosenet/datamanager.py
+++ diagnosenet/datamanager.py
@@ -32,10 +32,6 @@ class Dataset:
         self.inputs_name: str
         self.labels_name: str
 
-        self.train: DataSplit
-        self.valid: DataSplit
-        self.test: DataSplit
-
     def set_data_file(self, inputs: np.ndarray, targets: np.ndarray) -> None:
         """
         Set data file
@@ -77,20 +73,35 @@ class Dataset:
         else:
             raise NameError("targets_path not localized: {}".format(self.targets_path))
 
-    def dataset_split(self, valid: float = None, test: float = None) -> None:
+
+class Splitting(Dataset):
+    """
+    split dataset in Training, Valid and test,
+    and split into mini-batches give a size factor.
+    """
+    def __init__(self, valid_size: float = None, test_size: float = None) -> None:
+        super().__init__()
+        self.valid_size = valid_size
+        self.test_size = test_size
+
+        self.train: DataSplit
+        self.valid: DataSplit
+        self.test: DataSplit
+
+    def dataset_split(self) -> None:
         """
         split dataset in Training, Valid and test,
         and split into mini-batches give a size factor.
         """
         try:
-            prop1=valid+test
-            prop2=(test*1)/(valid+test)
+            prop1=self.valid_size+self.test_size
+            prop2=(self.test_size*1)/(self.valid_size+self.test_size)
         except TypeError:
             logger.warning('!!! Splitting proportions are missing !!!')
-            valid = 0.05; test = 0.10
-            logger.warning('!!! Set valid={} and test={} by the fault !!!'.format(valid, test))
-            prop1=valid+test
-            prop2=(test*1)/(valid+test)
+            valid = 0.05; self.test_size = 0.10
+            logger.warning('!!! Set valid_size={} and test_size={} by the fault !!!'.format(self.valid_size, self.test_size))
+            prop1=self.valid_size+self.test_size
+            prop2=(self.test_size*1)/(self.valid_size+self.test_size)
 
         X_train, X_temp, y_train, y_temp = train_test_split(self.inputs, self.targets, test_size=prop1)
         X_valid, X_test, y_valid, y_test = train_test_split(X_temp,y_temp, test_size=prop2)
@@ -107,15 +118,15 @@ class Dataset:
         logger.info('-- Test records:   {} | {} --'.format(len(self.test.inputs), len(self.test.targets)))
 
 
-
-class Batching(Dataset):
+class Batching(Splitting):
     """
     Write micro-batches by a size split factor
     for each part of the data, Training, Valid, Test
     """
-
-    def __init__(self):
-        super().__init__()
+    def __init__(self, valid_size: float = None, test_size: float = None,
+                        batch_size: int = 1000):
+        super().__init__(valid_size, test_size)
+        self.batch_size = batch_size
 
     def batching(self, data: DataSplit) -> Iterator[Batch]:
         """
@@ -129,25 +140,25 @@ class Batching(Dataset):
             batch_targets = data.targets[start:end]
             yield Batch(batch_inputs, batch_targets)
 
-    def memory_batching(self, batch_size: int = 1000, shuffle: bool = True,
-                    valid: float = None, test: float = None) -> Iterator[Batch]:
+    def memory_batching(self, shuffle: bool = True) -> Iterator[Batch]:
         """
         """
-        self.batch_size = batch_size
         self.shuffle = shuffle
-        self.dataset_split(valid, test)
+        ## Splittting the Dataset
+        self.dataset_split()
+
         train_batches = self.batching(self.train)
         valid_batches = self.batching(self.valid)
         test_batches = self.batching(self.test)
-        return train_batches, valid_batches, test_batches
+        # return train_batches, valid_batches, test_batches
+        return self.batching(self.train), self.batching(self.valid), self.batching(self.test)
 
-    def disk_batching(self, batch_size: int,
-                        valid: float = None, test: float = None) -> None:
-        self.batch_size = batch_size
-        self.dataset_split(valid, test)
+    def disk_batching(self) -> None:
+        ## Splittting the Dataset
+        self.dataset_split()
 
         ## Defining split directories
-        self.split_path = str(self.sandbox+"/2_Split_Point-"+str(batch_size))
+        self.split_path = str(self.sandbox+"/2_Split_Point-"+str(self.batch_size))
         train_path = str(self.split_path+"/data_training/")
         valid_path = str(self.split_path+"/data_valid/")
         test_path = str(self.split_path+"/data_test/")
@@ -168,12 +179,35 @@ class Batching(Dataset):
 
 class MultiTask(Batching):
     """
+    A multi task label set the
     """
+    def __init__(self, valid_size: float = None, test_size: float = None,
+                        batch_size: int = 1000,
+                        target_name: str = 'Y11',
+                        target_start: int = 0, target_end: int = 14) -> None:
+        super().__init__(valid_size, test_size, batch_size)
+        self.target_name = target_name
+        self.target_start = target_start
+        self.target_end = target_end
 
-    def __init__(self):
-        super().__init__()
+    def memory_target_splitting(self, batches: Iterator[Batch]) -> None:
+        for batch in batches:
+            batch_one_target = batch.targets[:,self.target_start:self.target_end]
+            yield Batch(batch.inputs, batch_one_target)
 
-    def target_splitting(self, splitting: str, start: float, end: float, label_name: str) -> None:
+    def memory_one_target(self) -> None:
+        """
+        This function splits and write the label selected from a multi-labels file.
+        In which each label has been write-in one-hot encode.
+        """
+        train_batches, valid_batches, test_batches = self.memory_batching()
+
+        train_batches = self.memory_target_splitting(train_batches)
+        valid_batches = self.memory_target_splitting(valid_batches)
+        test_batches = self.memory_target_splitting(test_batches)
+        return train_batches, valid_batches, test_batches
+
+    def disk_target_splitting(self, splitting: str) -> None:
 
         ## Defining split directories
         splitting_path = str(self.split_path+"/data_"+splitting)
@@ -186,12 +220,13 @@ class MultiTask(Batching):
             # ## Convert list in a numpy matrix
             data = pd.DataFrame(data)
             data = data[0].str.split(',').tolist()
+
             ## set the label selected
-            label = np.asarray(data)[:,start:end]
+            label = np.asarray(data)[:,self.target_start:self.target_end]
 
             ## Set file_name
             f_names = f.split('-')
-            path_to_file = str(splitting_path+"/y-"+self.dataset_name+"-"+label_name+"-"+f_names[-1])
+            path_to_file = str(splitting_path+"/y-"+self.dataset_name+"-"+self.target_name+"-"+f_names[-1])
 
             ## Write new label file
             # if path_to_file not in files:
@@ -199,13 +234,14 @@ class MultiTask(Batching):
                 np.savetxt(file_, label, fmt='%s',delimiter=',',newline='\n' )
             file_.close()
 
-    def set_one_target(self, label_name: str = "Y11",
-                                start: int = 0, end: int = 14) -> None:
+    def disk_one_target(self) -> None:
         """
         This function splits and write the label selected from a multi-labels file.
         In which each label has been write-in one-hot encode.
         """
+        ## Baching the dataset
+        self.disk_batching()
 
-        self.target_splitting("training", start, end, label_name)
-        self.target_splitting("valid", start, end, label_name)
-        self.target_splitting("test", start, end, label_name)
+        self.disk_target_splitting("training")
+        self.disk_target_splitting("valid")
+        self.disk_target_splitting("test")
