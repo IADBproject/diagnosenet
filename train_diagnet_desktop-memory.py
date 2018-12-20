a98e3e7faf13aa4b0e7d815d61d8a3ab3ffc4afe
jagh <jagh1729@gmail.com>
2018-12-20 22:43:44 +0100

    Medical care-purpose classification example on desktop-memory

--- train_diagnet_desktop-memory.py
+++ train_diagnet_desktop-memory.py
@@ -11,7 +11,7 @@ from diagnosenet.datamanager import MultiTask
 from diagnosenet.layers import Relu, Softmax
 from diagnosenet.losses import CrossEntropy
 from diagnosenet.optimizers import Adam
-from diagnosenet.graphs import MLP
+from diagnosenet.graphs import FullyConnected
 from diagnosenet.executors import DesktopExecution
 
 ### Read the PMSI-Dataset using Pickle from diagnosenet.io_functions
@@ -20,31 +20,36 @@ X = IO_Functions()._read_file(path+"BPPR-W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1-2008.tx
 y = IO_Functions()._read_file(path+"labels_Y1-W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
 
 ## 1) Define the stacked layers as the number of layers and their neurons
-staked_layers = [Relu(14637, 2048),
-                Relu(2048, 2048),
-                Relu(2048, 2048),
-                Relu(2048, 2048),
-                Relu(2048, 2048),
-                Softmax(2048, 14)]
+layers = [Relu(14637, 2048),
+            Relu(2048, 2048),
+            Relu(2048, 2048),
+            Relu(2048, 2048),
+            Relu(2048, 2048),
+            Softmax(2048, 14)]
 
 ## 2) Select the neural network architecture and pass the hyper-parameters
-mlp_model = MLP(input_size=14637, output_size=14,   #381,
-                layers=staked_layers,
+mlp_model = FullyConnected(input_size=14637, output_size=14,   #381,
+                layers=layers,
                 loss=CrossEntropy,
                 optimizer=Adam(lr=0.01))
 
 ## 3) Dataset configurations for splitting, batching and target selection
-data_config = MultiTask(batch_size=100,
-                        valid_size=0.05, test_size=0.10,
+data_config = MultiTask(valid_size=0.1, test_size=0.10,
+                        batch_size=100,
                         target_name='Y11',
                         target_start=0, target_end=14)
 
 ## 4) Select the computational platform and pass the DNN and Dataset configurations
 platform = DesktopExecution(model=mlp_model,
                             datamanager=data_config,
-                            max_epochs=10)
+                            max_epochs=1)
 
 ## 5) Uses the platform modes for training in an efficient way
-platform.training_memory(X, y)
+# platform.training_memory(X, y)
+
+## 6) Write metrcis
+platform.write_metrics(testbed_path='testbed')
+
+
 
 print("Execution Time: {}".format((time.time()-execution_start)))
