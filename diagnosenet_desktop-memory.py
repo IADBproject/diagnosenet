46e1d952e065df0046f080036daae19dbcade5ea
jagh <jagh1729@gmail.com>
2019-01-29 19:01:05 +0100

    Added a second sandbox for data exploration and model authoring

--- diagnosenet_desktop-memory.py
+++ diagnosenet_desktop-memory.py
@@ -17,37 +17,52 @@ from diagnosenet.monitor import enerGyPU
 
 
 ### Read the PMSI-Dataset using Pickle from diagnosenet.io_functions
-path = "healthData/sandbox-W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1/1_Mining-Stage/binary_representation/"
-X = IO_Functions()._read_file(path+"BPPR-W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
-y = IO_Functions()._read_file(path+"labels_Y1-W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+#path = "/data_B/datasets/drg-PACA/healthData/sandbox-FULL-W1_x1_x2_x3_x4_x5_x7_x8_Y1/1_Mining-Stage/binary_representation/"
+#X = IO_Functions()._read_file(path+"BPPR-FULL-W1_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+#y = IO_Functions()._read_file(path+"labels_Y1-FULL-W1_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+
+
+path = "/data_B/datasets/drg-PACA/healthData/sandbox-SENSE-CUSTOM_x1_x2_x3_x4_x5_x7_x8_Y1/1_Mining-Stage/binary_representation/"
+X = IO_Functions()._read_file(path+"BPPR-SENSE-CUSTOM_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+y = IO_Functions()._read_file(path+"labels_Y1-SENSE-CUSTOM_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+
+
+
+
 
 ## 1) Define the stacked layers as the number of layers and their neurons
-layers = [Relu(14637, 2048),
-            Relu(2048, 2048),
-            Relu(2048, 2048),
-            Relu(2048, 2048),
-            Linear(2048, 14)]
+layers = [Relu(10833, 1024),
+#layers = [Relu(14637, 2048),
+            Relu(1024, 1024),
+            Relu(1024, 1024),
+            Relu(1024, 1024),
+            Linear(1024, 14)]
+#            Linear(2048, 5)]
+            #Linear(2048, 239)]
 
 ## 2) Select the neural network architecture and pass the hyper-parameters
-mlp_model = FullyConnected(input_size=14637, output_size=14,   #239,
+#mlp_model = FullyConnected(input_size=14637, output_size=14,   #239,
+mlp_model = FullyConnected(input_size=10833, output_size=14,
                 layers=layers,
                 loss=CrossEntropy,
                 optimizer=Adam(lr=0.001),
                 dropout=0.8)
 
 ## 3) Dataset configurations for splitting, batching and target selection
-data_config = MultiTask(dataset_name="W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1",
-                        valid_size=0.05, test_size=0.10,
-                        batch_size=100,
+data_config = MultiTask(dataset_name="SENSE-CUSTOM_x1_x2_x3_x4_x5_x7_x8_Y1",
+                        valid_size=0.05, test_size=0.15,
+                        batch_size=100,	#3072,	#100,
                         target_name='Y11',
                         target_start=0, target_end=14)
+#                        target_start=239, target_end=244)
+                        #target_start=14, target_end=253) 
 
 ## 4) Select the computational platform and pass the DNN and Dataset configurations
 platform = DesktopExecution(model=mlp_model,
                             datamanager=data_config,
-                            monitor=enerGyPU(testbed_path="testbed"),
-                            max_epochs=2,
-                            min_loss=2.0)
+                            monitor=enerGyPU(testbed_path="/data/jagh/green_learning/testbed"),
+                            max_epochs=40,
+                            min_loss=0.02)
 
 ## 5) Uses the platform modes for training in an efficient way
 platform.training_memory(X, y)
