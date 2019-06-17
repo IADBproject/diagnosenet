f2b6a17a0da9fc8e939730ad674b0735efdcf1bf
jagh <jagh1729@gmail.com>
2019-06-17 17:48:57 +0200

    Example to medical care-purpose classification on memory processing

--- diagnosenet_desktop-memory.py
+++ diagnosenet_desktop-memory.py
@@ -15,48 +15,61 @@ from diagnosenet.graphs import FullyConnected
 from diagnosenet.executors import DesktopExecution
 from diagnosenet.monitor import enerGyPU
 
+
 ### Read the PMSI-Dataset using Pickle from diagnosenet.io_functions
 ## Path for Octopus Machine: Full Representation
+#path = "/data_B/datasets/drg-PACA/healthData/sandbox-FULL-W1_x1_x2_x3_x4_x5_x7_x8_Y1/1_Mining-Stage/binary_representation/"
+#X = IO_Functions()._read_file(path+"BPPR-FULL-W1_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+#y = IO_Functions()._read_file(path+"labels_Y1-FULL-W1_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+
+## Path for Octopus Machine: SENSE-CUSTOM Representation
+#path = "/data_B/datasets/drg-PACA/healthData/sandbox-SENSE-CUSTOM_x1_x2_x3_x4_x5_x7_x8_Y1/1_Mining-Stage/binary_representation/"
+#X = IO_Functions()._read_file(path+"BPPR-SENSE-CUSTOM_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+#y = IO_Functions()._read_file(path+"labels_Y1-SENSE-CUSTOM_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
 
 path = "healthData/sandbox-W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1/1_Mining-Stage/binary_representation/"
-X = IO_Functions()._read_file(path + 'BPPR-pre-trained-2019.txt')
-y = IO_Functions()._read_file(path + 'labels_Y3-pre-trained-2019.txt')
+X = IO_Functions()._read_file(path+"BPPR-W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+y = IO_Functions()._read_file(path+"labels_Y1-W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1-2008.txt")
+
 
 ## 1) Define the stacked layers as the number of layers and their neurons
-input_size = 38
-output_size = 5
-
-# layers_1 = [Relu(input_size, 2048),
-#             Relu(2048, 2048),
-#             Relu(2048, 1024),
-#             Relu(1024, 1024),
-#             Linear(1024, output_size)]
-
-layers_1 = [Relu(input_size, 100),
-            Relu(100, 100),
-            Relu(100, 50),
-            Relu(50, 50),
-            Linear(50, output_size)]
+layers_1 = [Relu(14637, 2048),
+            Relu(2048, 2048),
+            Relu(2048, 1024),
+            Relu(1024, 1024),
+            Linear(1024, 14)]
 
 ## 2) Select the neural network architecture and pass the hyper-parameters
-# mlp_model_1 = FullyConnected(input_size=input_size, output_size=output_size,   #239,
-#                 layers=layers_1,
-#                 loss=CrossEntropy,
-#                 optimizer=Adam(lr=0.001),
-#                 dropout=0.8)
-mlp_model_1 = FullyConnected(input_size=input_size, output_size=output_size,   #239,
+mlp_model_1 = FullyConnected(input_size=14637, output_size=14,   #239,
                 layers=layers_1,
                 loss=CrossEntropy,
                 optimizer=Adam(lr=0.001),
                 dropout=0.8)
 
+# ## Added a second model for a network architecture search
+# layers_2 = [Relu(10833, 1024),
+#             Relu(1024, 1024),
+#             Relu(1024, 1024),
+#             Relu(1024, 1024),
+#             Linear(1024, 14)] #MT1
+#             Linear(2048, 239)] #MT2
+#             Linear(2048, 5)] #MT3
+#
+# mlp_model_2 = FullyConnected(input_size=10833, output_size=14,
+#                 layers=layers_2,
+#                 loss=CrossEntropy,
+#                 optimizer=Adam(lr=0.001),
+#                 dropout=0.8)
+
+
 ## 3) Dataset configurations for splitting, batching and target selection
-dataset_name = 'MIMIC_x1_x2_x3_Y3'
-data_config = MultiTask(dataset_name=dataset_name,
-                        valid_size=0.3, test_size=0.2,
-                        batch_size=3,	#3072,	#100,
-                        target_name='Y3',
-                        target_start=0, target_end=output_size) #MT1
+data_config = MultiTask(dataset_name="W1-TEST_x1_x2_x3_x4_x5_x7_x8_Y1",
+                        valid_size=0.05, test_size=0.15,
+                        batch_size=100,	#3072,	#100,
+                        target_name='Y11',
+                        target_start=0, target_end=14) #MT1
+                        #target_start=14, target_end=253) #MT2
+                        #target_start=239, target_end=244) #MT3
 
 ## 4) Select the computational platform and pass the DNN and Dataset configurations
 platform = DesktopExecution(model=mlp_model_1,
@@ -67,5 +80,6 @@ platform = DesktopExecution(model=mlp_model_1,
 
 ## 5) Uses the platform modes for training in an efficient way
 platform.training_memory(X, y)
+
 
 print("Execution Time: {}".format((time.time()-execution_start)))
