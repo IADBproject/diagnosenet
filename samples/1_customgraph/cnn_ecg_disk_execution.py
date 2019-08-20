22514a2a5cffb8aada82748e2fd2353aae3d6b5b
jagh <jagh1729@gmail.com>
2019-08-20 20:07:00 +0200

    Updated executors for ECG medical target

--- samples/1_customgraph/cnn_ecg_disk_execution.py
+++ samples/1_customgraph/cnn_ecg_disk_execution.py
@@ -1,13 +1,10 @@
-
-
 import time
 execution_start = time.time()
 
 ## Makes diagnosenet library visible in samples folder
 import sys
-#file_path="../../"
-file_path = "/home/mpiuser/cloud/diagnosenet/"
-sys.path.append(file_path)
+workspace_path="../../"
+sys.path.append(workspace_path)
 
 from diagnosenet.datamanager import MultiTask, Batching
 from diagnosenet.layers import Relu, Linear
@@ -21,53 +18,24 @@ import pandas as pd
 
 
 
+layer_1=[Linear(1300, 4)]
 
-def main(argv):
-    ## Formating the argv from reseource manager
-    for i in range(len(argv)):
-        argv[i]=str(argv[i]).replace('[','').replace(']','').replace(',','')
-
-
-    ## Formatiing the workers IP to build
-    ## a tensorflow cluster in executor class
-    temp_workers = []
-    for i in range(len(argv)-3):
-        temp_workers.append(argv[i+3])
-    temp_workers = ','.join(temp_workers)
-
-
-    ## 1) Define stacked layers funtion activation, number of layers and their neurons
-    layer_1=[Linear(1300, 4)]
-
-
-    ## 2) Select the neural network architecture and pass the hyper-parameters
-    cnn_model = CustomGraph(input_size_1=1300,input_size_2=1, output_size=4,
+model = CustomGraph(input_size_1=1300,input_size_2=1, output_size=4,
                         loss=CrossEntropy,
                         optimizer=Adam(lr=0.0001),layers=layer_1)
 
-    ## 3) Dataset configurations for splitting, batching and target selection
-    data_config = Batching(dataset_name="ECG",
+data_config = Batching(dataset_name="ECG",
                         valid_size=0.1, test_size=0.1,
-                        batch_size=10, devices_number=1)
+                        batch_size=1000)
 
-    ## 4) Select the computational platform and pass the DNN and Dataset configurations
-    projection = Distibuted_GRPC(model=cnn_model,
+projection = DesktopExecution(model,
                         datamanager=data_config,
-                        monitor=enerGyPU(testbed_path="/home/mpiuser/cloud/diagnosenet/samples/1_customgraph/testbed",
-                                              machine_type="arm", file_path=file_path),
-                        max_epochs=2, min_loss=0.002,
-                        ip_ps=argv[2], ip_workers=temp_workers)
+                        monitor=enerGyPU(machine_type="x86", file_path=workspace_path),
+                        max_epochs=20, early_stopping=5)
 
-    ## 5) Uses the platform modes for training in an efficient way
-    projection.training_disk(dataset_name="ECG",
-                        dataset_path="/home/mpiuser/cloud/diagnosenet/samples/1_customgraph/dataset/",
-                        inputs_name="xdata.npy",
-                        targets_name="undim-ydata.npy",
-                        job_name=argv[0],
-                        task_index=int(argv[1]))
+projection.training_disk(dataset_name="ECG",
+                        dataset_path="dataset/",
+                        inputs_name="xdata-big.npy",
+                        targets_name="undim-ydata-big.npy")
 
-    print("Execution Time: {}".format((time.time()-execution_start)))
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
-
+print("Execution Time: {}".format((time.time()-execution_start)))
--- samples/1_customgraph/cnn_ecg_memory_execution.py
+++ samples/1_customgraph/cnn_ecg_memory_execution.py
@@ -3,8 +3,8 @@ execution_start = time.time()
 
 ## Makes diagnosenet library visible in samples folder
 import sys
-file_path="../../"
-sys.path.append(file_path)
+workspace_path="../../"
+sys.path.append(workspace_path)
 
 from diagnosenet.losses import CrossEntropy
 from diagnosenet.optimizers import Adam
@@ -18,24 +18,24 @@ import pandas as pd
 
 #load data 
 file_dir = "dataset/"
-inputs = np.load(file_dir+'xdata.npy')
-targets = np.load(file_dir+'ydata.npy')
+inputs = np.load(file_dir+'xdata-big.npy')
+targets = np.load(file_dir+'ydata-big.npy')
 targets=pd.get_dummies(targets).values
 
 layer_1=[Linear(1300, 4)]
-
-data_config = Batching(dataset_name="ECG",
-                        valid_size=0.1, test_size=0.1,
-                        batch_size=10)
 
 model = CustomGraph(input_size_1=1300,input_size_2=1, output_size=4,
                         loss=CrossEntropy,
                         optimizer=Adam(lr=0.0001),layers=layer_1)
 
+data_config = Batching(dataset_name="ECG",
+                        valid_size=0.1, test_size=0.1,
+                        batch_size=1000)
+
 projection = DesktopExecution(model,
                         datamanager=data_config,
-                        monitor=enerGyPU(machine_type="arm",file_path=file_path), 
-                        max_epochs=2, min_loss=0.5)
+                        monitor=enerGyPU(machine_type="x86", file_path=workspace_path),
+                        max_epochs=20, early_stopping=5)
 
 projection.training_memory(inputs, targets)
 
